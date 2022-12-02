075427733136f0338d8bdb5a9065060611239335
boara <boara@DESKTOP-7O0CN6J>
2022-12-02 20:13:49 +0900

    커밋

--- finalproject/src/main/java/com/kh/finalproject/controller/AdminController.java
+++ finalproject/src/main/java/com/kh/finalproject/controller/AdminController.java
@@ -2,15 +2,21 @@ package com.kh.finalproject.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.finalproject.entity.AdminDto;
 
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
 	
-	
+	//관리자 계정 로그인
 	@GetMapping("/login")
 	public String login() {
 		return "admin/login";
@@ -23,11 +29,32 @@ public class AdminController {
 	    
 		return "admin/inquiry";
 		
+	}
+	
+	//관리자 일반회원 목록
+//	@GetMapping("/memberList")
+//	public String memberList(Model model, 
+//			@RequestParam(required=false) String type,
+//			@RequestParam(required=false) String keyword) {
+//		boolean isSearch=type!=null& keyword!=null;
+//		if(isSearch) {
+//			//model.addAttribute("memberList", memberda)
+//		}
+		
+		
+		
+		
+	
 		
 	}
+			
+			 
+
+	
+	
 	
 	
 	
 	
 
-}
+
