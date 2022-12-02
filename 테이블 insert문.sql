9f6273e4543fa92ea4afa359a18a7a471e1f82bc
whawo <99001224+whawo@users.noreply.github.com>
2022-12-02 11:42:28 +0900

    Update 테이블 insert문.sql

--- 테이블 insert문.sql
+++ 테이블 insert문.sql
@@ -23,3 +23,16 @@ insert into pet(
 pet_no, member_id, pet_type, pet_name, 
 pet_gender, pet_breed, pet_birth, pet_weight) 
 values(pet_seq.nextval, 'test1233', '고양이', '테스트이름', '남', '품종', '2015-08-15', 4);
+
+--trainer
+insert into trainer
+(trainer_no, member_id, apply_no, trainer_profile, trainer_profile_content)
+values(trainer_seq.nextval, 'trainer1', 6,  '항상 밝게웃는 훈련사 입니다',
+'어쩌구 저쩌구 나는 이러이러한 훈련사이고 모ㅗ모모모모모모모 어쩌꾸 저저구');
+
+--apply
+insert into apply(
+apply_no, member_id, apply_date, apply_motive,
+apply_smoke, apply_active, apply_license, apply_status)
+values(apply_seq.nextval, 'trainer1', sysdate, '열심히 할게요',
+'N', '평일', null, 'Y');
