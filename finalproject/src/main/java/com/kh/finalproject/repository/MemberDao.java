3de48893d9ee04b8dd4c400a90b6cc7764ab81a1
Lee-seungjung <tmdwjd0295@naver.com>
2022-12-09 15:17:27 +0900

    회원 dao, daoImpl

--- finalproject/src/main/java/com/kh/finalproject/repository/MemberDao.java
+++ finalproject/src/main/java/com/kh/finalproject/repository/MemberDao.java
@@ -15,24 +15,25 @@ import com.kh.finalproject.entity.MemberImgDto;
 
 public interface MemberDao {
 	
+	//등록
 	//회원 등록
 	void insert(MemberDto memberDto);
-	
-    //회원 검색
-	List<MemberListVO>selectList(MemberListSearchVO vo);
-	
-	
 	//회원, 첨부파일 연결테이블 등록
 	void memberProfileInsert(MemberImgDto memberImgDto);
 	
-
+	//조회
+    //회원 검색
+	List<MemberListVO>selectList(MemberListSearchVO vo);
 	//회원 조회
 	MemberDto selectOne(String memberId);
-	
-	//수정(결제 후 포인트 증가 처리)
-	boolean pointPlus(MemberDto memberDto);
-
-     //회원 프로필 이미지 파일 번호 찾기
+	//회원 프로필 이미지 파일 번호 찾기
 	int findFileNo(String memberId);
+	
+	//수정
+	//결제 후 포인트 증가 처리
+	boolean pointPlus(MemberDto memberDto);
+	//프로필 정보수정
+	boolean profileEdit(MemberDto memberDto);
+     
 
 }
--- finalproject/src/main/java/com/kh/finalproject/repository/MemberDaoImpl.java
+++ finalproject/src/main/java/com/kh/finalproject/repository/MemberDaoImpl.java
@@ -1,18 +1,16 @@
 package com.kh.finalproject.repository;
 
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import com.kh.finalproject.entity.MemberDto;
+import com.kh.finalproject.entity.MemberImgDto;
 import com.kh.finalproject.vo.MemberListSearchVO;
 import com.kh.finalproject.vo.MemberListVO;
-
-import com.kh.finalproject.entity.MemberImgDto;
 
 
 @Repository
@@ -26,31 +24,24 @@ public class MemberDaoImpl implements MemberDao {
 	public void insert(MemberDto memberDto) {
 		sqlSession.insert("member.insert", memberDto);
 	}
-
-
-
-
-	@Override
-	public List<MemberListVO> selectList(MemberListSearchVO vo) {
-	if(vo.isSearch()) {
-		return sqlSession.selectList("member.search",vo);
-	}
-	else {
-		return sqlSession.selectList("member.list",vo);
-	}
-	}
-
-
-
-	
 	//회원 프로필 등록
 	@Override
 	public void memberProfileInsert(MemberImgDto memberImgDto) {
 		sqlSession.insert("member.profileInsert", memberImgDto);
 	}
 	
-	//회원 단일조회
+	//회원 검색
+	@Override
+	public List<MemberListVO> selectList(MemberListSearchVO vo) {
+		if(vo.isSearch()) {
+			return sqlSession.selectList("member.search",vo);
+		}
+		else {
+			return sqlSession.selectList("member.list",vo);
+		}
+	}
 
+	//회원 단일조회
 	@Override
 	public MemberDto selectOne(String memberId) {
 		return sqlSession.selectOne("member.one", memberId);
@@ -61,10 +52,18 @@ public class MemberDaoImpl implements MemberDao {
 	public boolean pointPlus(MemberDto memberDto) {
 		return sqlSession.update("member.plusPoint",memberDto)>0;
 	}
-
+	
+	//회원 프로필 이미지 파일 번호 찾기
      @Override
 	public int findFileNo(String memberId) {		
 		return sqlSession.selectOne("member.findFileNo", memberId);
 	}
+     
+     //프로필 정보수정
+     @Override
+    public boolean profileEdit(MemberDto memberDto) {
+    	// TODO Auto-generated method stub
+    	return false;
+    }
 
 }
