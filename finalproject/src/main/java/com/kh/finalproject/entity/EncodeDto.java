1603e41a9fd835d35b99341de20f21d5a6949f4f
kind0312 <kind1207@naver.com>
2022-12-09 15:16:19 +0900

    암호화 구현 중

---
+++ finalproject/src/main/java/com/kh/finalproject/entity/EncodeDto.java
@@ -0,0 +1,12 @@
+package com.kh.finalproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class EncodeDto {
+	private String memberId;
+	private String memberPw;
+}
--- finalproject/src/main/java/com/kh/finalproject/repository/MemberDaoImpl.java
+++ finalproject/src/main/java/com/kh/finalproject/repository/MemberDaoImpl.java
@@ -1,18 +1,16 @@
 package com.kh.finalproject.repository;
 
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import com.kh.finalproject.entity.MemberDto;
+import com.kh.finalproject.entity.MemberImgDto;
 import com.kh.finalproject.vo.MemberListSearchVO;
 import com.kh.finalproject.vo.MemberListVO;
-
-import com.kh.finalproject.entity.MemberImgDto;
 
 
 @Repository
@@ -50,7 +48,6 @@ public class MemberDaoImpl implements MemberDao {
 	}
 	
 	//회원 단일조회
-
 	@Override
 	public MemberDto selectOne(String memberId) {
 		return sqlSession.selectOne("member.one", memberId);
