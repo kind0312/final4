b0a1233912d6447b435a390168961c7b7edeb680
leeseungjung <tmdwjd0295@naver.com>
2022-12-10 15:33:10 +0900

    .

--- finalproject/src/main/java/com/kh/finalproject/controller/TrainingController.java
+++ finalproject/src/main/java/com/kh/finalproject/controller/TrainingController.java
@@ -1,15 +1,9 @@
 package com.kh.finalproject.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import com.kh.finalproject.repository.TrainingDao;
 
 @Controller
 public class TrainingController {
 
-	@Autowired
-	private TrainingDao trainingDao;
-
-
+	
 }
