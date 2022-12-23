46ce0f23ebb6ab949091c89732e0fbea2ab5415f
kind0312 <kind1207@naver.com>
2022-12-23 16:15:59 +0900

    properties 수정

--- finalproject/src/main/java/com/kh/finalproject/restcontroller/MypageRestController.java
+++ finalproject/src/main/java/com/kh/finalproject/restcontroller/MypageRestController.java
@@ -1,14 +1,7 @@
 package com.kh.finalproject.restcontroller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.kh.finalproject.entity.PointDto;
-import com.kh.finalproject.repository.PointDao;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
