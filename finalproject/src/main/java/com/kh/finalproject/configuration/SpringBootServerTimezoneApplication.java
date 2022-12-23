d05061fde332cb49bcce7da1516307722c659551
kind0312 <kind1207@naver.com>
2022-12-23 16:30:35 +0900

    ServerTimezone 주석 추가

--- finalproject/src/main/java/com/kh/finalproject/configuration/SpringBootServerTimezoneApplication.java
+++ finalproject/src/main/java/com/kh/finalproject/configuration/SpringBootServerTimezoneApplication.java
@@ -1,11 +1,9 @@
 package com.kh.finalproject.configuration;
 
-import java.time.LocalDateTime;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -15,8 +13,8 @@ public class SpringBootServerTimezoneApplication {
     public void start() {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC")); 
     }
-    public static void main(String[] args) {       
-      SpringApplication.run(SpringBootServerTimezoneApplication.class, args);
-      
-    }
+//    public static void main(String[] args) {       
+//      SpringApplication.run(SpringBootServerTimezoneApplication.class, args);
+//      
+//    }
 }
